6f83320c50315d151ec97b9269fe25d7a3644617
daniel.ritter <daniel.ritter@reservix.de>
2021-09-06 14:13:21 +0200

    Exercise 01 extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,31 +3,20 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView, fetchPokemon} from '../pokemon'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary, PokemonInfoFallback} from '../pokemon'
+import {createResource} from 'utils'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
-// 🐨 create a variable called "pokemon" (using let)
-let pokemon;
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-const pokemonPromise = fetchPokemon('pikachu').then(result => {
-  pokemon = result
-}, error => {
-  console.error(error)
-});
+const resource = createResource(fetchPokemon('pikachu'))
 
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
 
 function PokemonInfo() {
+  const pokemon = resource.read()
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
-  if (!pokemon) {
-    throw pokemonPromise
-  }
 
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
@@ -45,8 +34,10 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={<div>loading...</div>}>
-          <PokemonInfo />
+        <React.Suspense fallback={<PokemonInfoFallback />}>
+          <PokemonErrorBoundary>
+            <PokemonInfo />
+          </PokemonErrorBoundary>
         </React.Suspense>
       </div>
     </div>
